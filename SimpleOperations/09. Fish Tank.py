e5541042e1dd5331e656a26e3640f39d35cb1d12
Georgi <49494430+Weongope@users.noreply.github.com>
2019-11-25 22:19:51 +0200

    Add files via upload

---
+++ SimpleOperations/09. Fish Tank.py
@@ -0,0 +1,12 @@
+l = int(input())
+w = int(input())
+h = int(input())
+percent = float(input())
+
+result = l * w * h
+result = result / 1000
+percent = percent * 0.01
+
+result = result * (1 - percent)
+
+print(f"{result:.3f}")
