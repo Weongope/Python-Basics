1f13a5d661203b3f7edf873da2f7605569dc7bb7
Georgi <49494430+Weongope@users.noreply.github.com>
2020-01-27 21:16:55 +0200

    Update Make_twenty.py

--- SimpleOperations/Make_twenty.py
+++ SimpleOperations/Make_twenty.py
@@ -1,5 +1,5 @@
 #Given two integers, return True if the sum of the integers is 20 or if one of the integers is 20. If not, return False
-def make_twenty(a,b):
+def make_twenty(n1,n2):
 	return (n1+n2) == 20 or n1 == 20 or n2 == 20
 
 print(make_twenty(2,18))
