ce4f8e8d72cdb51b3a3d32bd055cb824817f6a97
Georgi <49494430+Weongope@users.noreply.github.com>
2020-01-27 21:04:21 +0200

    Update master_yoda.py

--- SimpleOperations/master_yoda.py
+++ SimpleOperations/master_yoda.py
@@ -1,8 +1,7 @@
 def master_yoda(text):
-  yoda_text =[]
-  split_text = text.split(' ')
-  for element in split_text[::-1]:
-    yoda_text.append(element)
+  split_text = text.split()
+  yoda_text = split_text[::-1]
+    
   return " ".join(yoda_text)
   
-print(master_yoda("why dont you stay when you are so powerful"))
+print(master_yoda("May the force be with you"))
