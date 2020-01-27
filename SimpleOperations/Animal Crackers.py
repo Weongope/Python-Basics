9e55c7cc326b7aaf192fa7cb417f8b82d5a65ef6
Georgi <49494430+Weongope@users.noreply.github.com>
2020-01-27 20:29:17 +0200

    Change lower() to be only in the first half

--- SimpleOperations/Animal Crackers.py
+++ SimpleOperations/Animal Crackers.py
@@ -1,5 +1,6 @@
+#Write a function takes a two-word string and returns True if both words begin with same letter
 def animal_crackers(text):
-    b = text.split(" ")
-    return b[0][0].lower() == b[1][0].lower()
+    b = text.lower().split()
+    return b[0][0] == b[1][0]
        
 animal_crackers("LevelHeaded Cama")
