aa32cd2d0903e44228119e16c04210d5cd0f08b5
Georgi <49494430+Weongope@users.noreply.github.com>
2020-01-29 19:45:45 +0200

    Update and rename SimpleOperations/08. Yard Greening.py to SimpleOperations/First/yard_greening.py

---
+++ SimpleOperations/First/yard_greening.py
@@ -0,0 +1,9 @@
+#How much does gardening cost 
+yard = float(input())
+
+total_price = yard * 7.61
+
+disc = total_price * 0.18
+price = total_price - disc
+
+print(f"The final price is: {price:.2f} lv.\nThe discount is: {disc:.2f} lv.")
