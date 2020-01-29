95a7495ca000cd83139517123e16a1265219ceb4
Georgi <49494430+Weongope@users.noreply.github.com>
2020-01-29 19:57:52 +0200

    Update spy_game.py

--- SimpleOperations/spy_game.py
+++ SimpleOperations/spy_game.py
@@ -1,10 +1,8 @@
-nums = [7,0,3,0,7,3,7]
-agent = ""
+def spy_game(nums):
+	agent = ""
+	for i in range(0, len(nums)):
+		if nums[i] == 0 or (nums[i] == 7 and len(agent) > 0):
+			agent += str(nums[i])
+	return ("007" in agent)
 
-for i in range(0,len(nums)):
-  
-  if (nums[i]==0) or (nums[i]==7 and len(agent)>0):   
-    agent += str(nums[i])
-   
-    
-print(agent[:3] == "007")
+print(spy_game([7, 0, 3, 0, 0, 7, 3, 7]))
