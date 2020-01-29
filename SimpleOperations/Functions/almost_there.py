ac8ca2b6fa465d9fc633c1efe7aa36a9eeb6078e
Georgi <49494430+Weongope@users.noreply.github.com>
2020-01-29 19:42:21 +0200

    Update and rename SimpleOperations/almost_there.py to SimpleOperations/Functions/almost_there.py

---
+++ SimpleOperations/Functions/almost_there.py
@@ -0,0 +1,5 @@
+#return true if n is between 100 and 200 with 10
+def almost_there(n):
+  return ((n>90 and n<110) or (n>190 and n<210))
+
+print(almost_there(209))
--- SimpleOperations/almost_there.py
+++
@@ -1,4 +0,0 @@
-def almost_there(n):
-  return ((n>90 and n<110) or (n>190 and n<210))
-
-print(almost_there(209))
