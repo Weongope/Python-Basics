9de38e9f4005bf4dbcb54a3f46c592137a81c936
Georgi <49494430+Weongope@users.noreply.github.com>
2020-02-03 19:28:03 +0200

    Update unique_list.py

--- SimpleOperations/Functions/unique_list.py
+++ SimpleOperations/Functions/unique_list.py
@@ -1,11 +1,8 @@
 def unique_list(lst):
 	uniquelist =[]
 	for element in lst:
-		if element in uniquelist:
-			continue
-		else:
+		if element not in uniquelist:
 			uniquelist.append(element)
-
 	return uniquelist
 
 print(unique_list([1,2,3,4,4,4,4,1,2,3,6,5]))
