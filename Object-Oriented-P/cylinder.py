9bfeac826281f3ec85240885c464a38c9704e70b
Georgi <49494430+Weongope@users.noreply.github.com>
2020-02-07 22:55:50 +0200

    Create cylinder.py

---
+++ Object-Oriented-P/cylinder.py
@@ -0,0 +1,18 @@
+class Cylinder():
+
+   # Cylinder.pi = 3.14
+
+    def __init__(self, height = 1, radius = 1):
+        self.height = height
+        self.radius = radius
+
+
+    def volume(self):
+        return 3.14 * self.radius ** 2 * self.height
+
+    def surface_area(self):
+        return (2 * 3.14 * self.radius) * (self.radius + self.height)
+
+c = Cylinder(2,3)
+print(c.surface_area())
+
